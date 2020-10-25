c5e34128d8c4d0ea1157a83979378e0bf9f231c8
hyengchan <hyengchan@gmail.com>
2020-10-25 20:39:24 +0900

    prepare to start next chapter

---
+++ Chapter5/Chapter5_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chapter5
+//
+//  Created by 윤형찬 on 2020/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
